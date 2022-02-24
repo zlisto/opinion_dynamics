a5f9100c84ec7bdd06636768274273c1dc2a61a0
ysghysgh <99773507+ysghysgh@users.noreply.github.com>
2022-02-24 16:16:06 -0500

    Package of helper functions

---
+++ Helper_Functions.py
@@ -0,0 +1,44 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Helper_Functions.py",
+      "provenance": [],
+      "collapsed_sections": [],
+      "authorship_tag": "ABX9TyNP0ItvthNGUdLT+xs2clIx",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/zlisto/opinion_dynamics/blob/main/Helper_Functions.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 2,
+      "metadata": {
+        "id": "Wy9dgAnFg3VO"
+      },
+      "outputs": [],
+      "source": [
+        "def test0(a:int):\n",
+        "  return a+1"
+      ]
+    }
+  ]
+}
